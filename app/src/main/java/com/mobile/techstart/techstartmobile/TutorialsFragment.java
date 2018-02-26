21af6b7235eea3afbe296bb38eea8445f3763f8b
fifostack <darnell_luke@ymail.com>
2018-02-26 18:36:22 -0500

    Login Activity added (no auth yet)

--- app/src/main/java/com/mobile/techstart/techstartmobile/TutorialsFragment.java
+++ app/src/main/java/com/mobile/techstart/techstartmobile/TutorialsFragment.java
@@ -1,10 +1,12 @@
 package com.mobile.techstart.techstartmobile;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,17 +25,24 @@ import com.google.android.youtube.player.YouTubePlayerSupportFragment;
  * Created by Lucas on 2/2/2018.
  */
 
-public class TutorialsFragment extends Fragment implements YouTubePlayer.OnInitializedListener {
+public class TutorialsFragment extends Fragment implements YouTubePlayer.OnInitializedListener, View.OnClickListener {
 
     View myView;
     private static String video_id = "dIe9Zty-sw8";
+    private Button vButton1, vButton2, vButton3;
     private YouTubePlayer ytPlayer;
-
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.tutorials_layout, container, false);
+
+        vButton1 = myView.findViewById(R.id.videoButton1);
+        vButton1.setOnClickListener(this);
+        vButton2 = myView.findViewById(R.id.videoButton2);
+        vButton2.setOnClickListener(this);
+        vButton3 = myView.findViewById(R.id.videoButton3);
+        vButton3.setOnClickListener(this);
 
         YouTubePlayerFragment youtubePlayerFragment = new YouTubePlayerFragment();
         youtubePlayerFragment.initialize(Config.getKey(), this);
@@ -49,8 +58,10 @@ public class TutorialsFragment extends Fragment implements YouTubePlayer.OnIniti
     @Override
     public void onInitializationSuccess(Provider provider, YouTubePlayer youTubePlayer, boolean b) {
 
-        youTubePlayer.loadVideo(video_id);
-        youTubePlayer.play();
+        ytPlayer = youTubePlayer;
+        youTubePlayer.cueVideo(video_id);
+        Snackbar.make(myView,"YouTubePlayer Initialized", Snackbar.LENGTH_LONG )
+                .setAction("Action", null).show();
 
     }
 
@@ -61,5 +72,28 @@ public class TutorialsFragment extends Fragment implements YouTubePlayer.OnIniti
         Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_LONG).show();
         Log.d("errorMessage:", errorMessage);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()) {
+            case R.id.videoButton1:
+                ytPlayer.loadVideo("lpDIocpW6Nw");
+                break;
+            case R.id.videoButton2:
+                ytPlayer.loadVideo("P3MBQciFeHo");
+                break;
+            case R.id.videoButton3:
+                ytPlayer.loadVideo("dIe9Zty-sw8");
+                break;
+        }
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ytPlayer.pause();
     }
 }
